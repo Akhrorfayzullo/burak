f2fa2485f132dbdf457ce2f67d692b54efe8a81b
Akhrorfayzullo <akhrorfayzullo@gmail.com>
2024-06-09 19:26:39 +0900

    Train Task H

--- train.ts
+++ train.ts
@@ -1,4 +1,13 @@
-function getPositive(arr: number[]): string {
-  return arr.filter(num => num > 0).join('');
+//Task H
+function getDigits(input: string): string {
+  let result = '';
+  for (let a of input) {
+      if (a >= '0' && a <= '9') {
+          result += a;
+      }
+  }
+  return result;
 }
-console.log(getPositive([1, -4, 2]));    //
+
+const result = getDigits("m14i1t");
+console.log(result); 
