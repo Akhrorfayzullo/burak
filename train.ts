f6c9d1965f8744c26808fcb2161611c8ccdfccc2
Akhrorfayzullo <akhrorfayzullo@gmail.com>
2024-06-16 18:38:03 +0900

    train task

--- train.ts
+++ train.ts
@@ -1,13 +1,26 @@
-//Project standarts => Logging standarts, Naming standarts(function, variable, method =>CamelCase
-  //pascal => class
-  //folder => KEBAB
-  //css => snake
-//) 
-
-//Error handling
+function majorityElement(nums: number[]): number {
+  // Create a Map to count occurrences of each number
+  const mapCount = new Map<number, number>();
 
 
+  for (const num of nums) {
+      mapCount.set(num, (mapCount.get(num) || 0) + 1);
+  }
 
-//Traditional API
-//Rest API
-//GraphQL API
+
+  let majorityNum = nums[0];
+  let maxCount = 0;
+
+  for (const [num, count] of mapCount) {
+      if (count > maxCount) {
+          maxCount = count;
+          majorityNum = num;
+      }
+  }
+
+  return majorityNum;
+}
+
+
+console.log(majorityElement([1, 2, 3, 4, 5, 4, 3, 4])); 
+
