bf03a8abef87a4666059f56c71708897b50022e9
Akhrorfayzullo <akhrorfayzullo@gmail.com>
2024-06-16 18:22:04 +0900

    fix: modify restaurant login and sign up process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -2,6 +2,7 @@ import MemberModel from "../schema/Member.model";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
+import * as bcrypt from "bcryptjs";
 
 class MemberService {
 	private readonly memberModel;
@@ -15,6 +16,11 @@ class MemberService {
 			.exec();
 		console.log("exist", exist)
 		if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+		console.log("before", input.memberPassword)
+		const salt = await bcrypt.genSalt()
+		input.memberPassword = await bcrypt.hash(input.memberPassword,salt)
+		console.log("after", input.memberPassword)
+
 		try {
 			// console.log("passed here")
 			const result = await this.memberModel.create(input);
@@ -34,7 +40,12 @@ class MemberService {
 			.exec();
 		if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK)
 
-		const isMatch = input.memberPassword === member.memberPassword	
+		const isMatch = await bcrypt.compare(
+			input.memberPassword,
+			member.memberPassword
+
+		)
+		// const isMatch = input.memberPassword === member.memberPassword	
 		// console.log("match", isMatch)
 		if (!isMatch)
 			throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD)
