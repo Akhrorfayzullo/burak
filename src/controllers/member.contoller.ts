49d1951aff5210e1c67ff98a4a222859177209a9
Akhrorfayzullo <akhrorfayzullo@gmail.com>
2024-06-20 19:14:58 +0900

    fix:user Login

--- src/controllers/member.contoller.ts
+++ src/controllers/member.contoller.ts
@@ -34,11 +34,11 @@ memberController.login = async (req: Request, res: Response) => {
         const input : LoginInput = req.body,
           result = await memberService.login(input);
         res.json({member : result});
-    } catch (err){
-
-        console.log("ERROR , login: ",err)
-        // res.json(err)
-    }
+    } catch (err: any) {
+		console.log("Error on login", err.message);
+		if (err instanceof Errors) res.status(err.code).json(err);
+		else res.status(Errors.standard.code).json(Errors.standard.message);		
+	}
 }
 
 // *************************
