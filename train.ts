87f1111860ebdc8814a94c829cf43d57a142b950
Akhrorfayzullo <akhrorfayzullo@gmail.com>
2024-06-28 19:51:59 +0900

    Fix: frontend public and view files

--- train.ts
+++ train.ts
@@ -3,3 +3,17 @@
 //update Chosen users
  
 
+const palindromCheck = (word: string): boolean => {
+    
+    const cleanedWord = word.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+    const reversedWord = cleanedWord.split('').reverse().join('');
+
+    
+    return cleanedWord === reversedWord;
+}
+
+
+console.log(palindromCheck("dad")); 
+console.log(palindromCheck("son"));
+console.log(palindromCheck("Falastin")); 
