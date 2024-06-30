74374e12c21321099078d277316995bfab9f460a
Akhrorfayzullo <akhrorfayzullo@gmail.com>
2024-06-30 17:15:02 +0900

    fix

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -65,7 +65,7 @@ restaurantController.processLogin = async (req: AdminRequest, res: Response) => 
         const message =
 			err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
 		res.send(
-			`<script>alert(${message}); window.location.replace("admin/login")</script>`
+			`<script>alert(${message}); window.location.replace("/admin/login")</script>`
 		);
     }
 }
@@ -101,7 +101,7 @@ restaurantController.processSignup = async (req: AdminRequest, res: Response) =>
         const message =
 			err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
 		res.send(
-			`<script>alert(${message}); window.location.replace("admin/signup")</script>`
+			`<script>alert(${message}); window.location.replace("/admin/signup")</script>`
 		);
     }
 }
