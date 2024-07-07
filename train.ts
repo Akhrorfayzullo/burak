50ccfb68ee1854bbf392ce9dce83102eab1f053c
Akhrorfayzullo <akhrorfayzullo@gmail.com>
2024-07-07 14:31:24 +0900

    train

--- train.ts
+++ train.ts
@@ -1,7 +1,12 @@
-const hasProperty = (obj: { [key: string]: any }, prop: string): boolean => {
-    return prop in obj;
+const calculate = (num: string): number => {
+    const parts = num.split('+');
+    
+    const sum = parts.reduce((total, num) => total + parseFloat(num), 0);
+    
+    return sum;
 }
 
-console.log(hasProperty({name: "BMW", model: "M3"}, "model"));
-console.log(hasProperty({name: "BMW", model: "M3"}, "year"));  
+console.log(calculate("10+20+30")); 
+console.log(calculate("5.5+4.5"));
+  
 
