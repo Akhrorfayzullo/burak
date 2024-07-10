a2826ec5a5f4f38934ee6090cd40f57366d46b3e
Akhrorfayzullo <akhrorfayzullo@gmail.com>
2024-07-10 20:34:41 +0900

    train

--- train.ts
+++ train.ts
@@ -1,12 +1,11 @@
-const calculate = (num: string): number => {
-    const parts = num.split('+');
-    
-    const sum = parts.reduce((total, num) => total + parseFloat(num), 0);
-    
-    return sum;
+const missingNumber = (nums: number[]): number => {
+    const n = nums.length;
+    const totalSum = (n * (n + 1)) / 2;
+    const arraySum = nums.reduce((acc, num) => acc + num, 0);
+    return totalSum - arraySum;
 }
 
-console.log(calculate("10+20+30")); 
-console.log(calculate("5.5+4.5"));
-  
+console.log(missingNumber([3, 0, 1])); 
+console.log(missingNumber([0, 1]));   
+
 
