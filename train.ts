56a77173ef1a8c9930f1c14458aada5b8e6dea8f
Akhrorfayzullo <akhrorfayzullo@gmail.com>
2024-07-17 20:43:08 +0900

    Fix: train

--- train.ts
+++ train.ts
@@ -1,8 +1,17 @@
-const mergeSortedArrays = (arr1: number[], arr2: number[]): number[] => {
-    return [...arr1, ...arr2].sort((a, b) => a - b);
+const countChars = (str: string): { [key: string]: number } => {
+    const charCount: { [key: string]: number } = {};
+
+    for (const char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    return charCount;
 }
 
-console.log(mergeSortedArrays([0, 3, 4, 31], [4, 6, 30])); 
+console.log(countChars("hello")); 
+console.log(countChars("test")); 
+console.log(countChars("aabbcc")); 
+
 
 
 
